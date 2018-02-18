c1731eed40d2ef33a68e3378559412e9ae9d0196
areeg94fahad <fahedareeg@gmail.com>
2018-02-18 23:41:35 +0300

    some init msg

---
+++ script.js
@@ -0,0 +1,18 @@
+function mouselog(event) {
+
+
+var n=event.target.className;
+  text.value =  event.target.className
+
+  text.scrollTop = text.scrollHeight
+
+  if((text.value!="blue")&&(text.value!="red29")&&(text.value!="red30")){
+    window.alert("lose plase refrash page again or just press f5");}
+    if(text.value=="red30"){
+
+        window.alert("you won wooo ");
+    }
+
+
+
+}
